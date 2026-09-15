668b2e0ac14be900bb9bc8b4788342ba984a82e6
ChavalBenjamin <benjaminchaval@gmail.com>
2026-09-15 22:01:28 +0000

    Remplacement effectif des fichiers OperateursSpectraux par la version avec les 5 parametres

--- OperateursSpectraux/OperateursSpectraux.cpp
+++ OperateursSpectraux/OperateursSpectraux.cpp
@@ -1,58 +1,109 @@
 #include "OperateursSpectraux.h"
 #include "IPlug_include_in_plug_src.h"
-
-#if IPLUG_EDITOR
 #include "IControls.h"
-#endif
 
 OperateursSpectraux::OperateursSpectraux(const InstanceInfo& info)
-: iplug::Plugin(info, MakeConfig(kNumParams, kNumPresets))
+: iplug::Plugin(info, MakeConfig(kNumParams, 1))
 {
-  GetParam(kParamGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%");
+  GetParam(kParamCycles)->InitDouble("Cycles", 1., 0., 24., 0.01);
+  GetParam(kParamQ)->InitPercentage("Q", 50.); // 50% = milieu = sinus plein par defaut
+  GetParam(kParamBallade)->InitPercentage("Ballade", 0.);
+  GetParam(kParamHorizon)->InitPercentage("Horizon", 50.); // 50% = symetrique par defaut
+  GetParam(kParamSkew)->InitDouble("Skew", 1., 0.1, 6., 0.01);
 
-#if IPLUG_EDITOR // http://bit.ly/2S64BDd
+#if IPLUG_EDITOR
   mMakeGraphicsFunc = [&]() {
-    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS);
+    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS,
+                         GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
   };
-  
+
   mLayoutFunc = [&](IGraphics* pGraphics) {
-    const IRECT bounds = pGraphics->GetBounds();
-    const IRECT innerBounds = bounds.GetPadded(-10.f);
-    const IRECT sliderBounds = innerBounds.GetFromLeft(150).GetMidVPadded(100);
-    const IRECT versionBounds = innerBounds.GetFromTRHC(300, 20);
-    const IRECT titleBounds = innerBounds.GetCentredInside(200, 50);
-
-    if (pGraphics->NControls()) {
-      pGraphics->GetBackgroundControl()->SetTargetAndDrawRECTs(bounds);
-      pGraphics->GetControlWithTag(kCtrlTagSlider)->SetTargetAndDrawRECTs(sliderBounds);
-      pGraphics->GetControlWithTag(kCtrlTagTitle)->SetTargetAndDrawRECTs(titleBounds);
-      pGraphics->GetControlWithTag(kCtrlTagVersionNumber)->SetTargetAndDrawRECTs(versionBounds);
-      return;
-    }
-
-    pGraphics->SetLayoutOnResize(true);
-    pGraphics->AttachCornerResizer(EUIResizerMode::Size, true);
+    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+    pGraphics->AttachPanelBackground(COLOR_GRAY);
     pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
-    pGraphics->AttachPanelBackground(COLOR_LIGHT_GRAY);
-    pGraphics->AttachControl(new IVSliderControl(sliderBounds, kParamGain), kCtrlTagSlider);
-    pGraphics->AttachControl(new ITextControl(titleBounds, "OperateursSpectraux", IText(30)), kCtrlTagTitle);
-    WDL_String buildInfoStr;
-    GetBuildInfoStr(buildInfoStr, __DATE__, __TIME__);
-    pGraphics->AttachControl(new ITextControl(versionBounds, buildInfoStr.Get(), DEFAULT_TEXT.WithAlign(EAlign::Far)), kCtrlTagVersionNumber);
+
+    const IVStyle knobStyle = DEFAULT_STYLE.WithLabelText(IText(10.f, COLOR_WHITE));
+
+    const IRECT bounds = pGraphics->GetBounds();
+    IRECT controlsRow = bounds.GetFromTop(120.f).GetPadded(-15.f);
+
+    pGraphics->AttachControl(new IVKnobControl(controlsRow.GetGridCell(0, 0, 1, 5).GetCentredInside(80.f), kParamCycles, "Cycles", knobStyle));
+    pGraphics->AttachControl(new IVKnobControl(controlsRow.GetGridCell(0, 1, 1, 5).GetCentredInside(80.f), kParamQ, "Q", knobStyle));
+    pGraphics->AttachControl(new IVKnobControl(controlsRow.GetGridCell(0, 2, 1, 5).GetCentredInside(80.f), kParamBallade, "Ballade", knobStyle));
+    pGraphics->AttachControl(new IVKnobControl(controlsRow.GetGridCell(0, 3, 1, 5).GetCentredInside(80.f), kParamHorizon, "Horizon", knobStyle));
+    pGraphics->AttachControl(new IVKnobControl(controlsRow.GetGridCell(0, 4, 1, 5).GetCentredInside(80.f), kParamSkew, "Skew", knobStyle));
+
+    IRECT curveArea = IRECT(bounds.L, bounds.T + 120.f, bounds.R, bounds.B).GetPadded(-20.f);
+    mCurveView = new SpectralCurvePreviewControl(curveArea);
+    pGraphics->AttachControl(mCurveView);
   };
+#endif
+
+#if IPLUG_DSP
+  OnReset();
+#endif
+}
+
+void OperateursSpectraux::OnIdle()
+{
+#if IPLUG_DSP
+  if (mCurveView && mCurveUIUpdated.exchange(false))
+  {
+    mCurveView->SetCurve(mCurveUIBuf, mCurveUISize);
+    mCurveView->SetDirty(false);
+  }
 #endif
 }
 
 #if IPLUG_DSP
-void OperateursSpectraux::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
-{ 
-  const int nChans = NOutChansConnected();
-  const double gain = GetParam(kParamGain)->Value() / 100.;
-  
-  for (int s = 0; s < nFrames; s++) {
-    for (int c = 0; c < nChans; c++) {
-      outputs[c][s] = inputs[c][s] * gain;
-    }
+
+void OperateursSpectraux::UpdateEngine()
+{
+  mEngine.SetSize(512); // resolution de l'apercu pour l'instant (etape 1, pas encore lie a une taille FFT)
+  mEngine.SetCycles((float)GetParam(kParamCycles)->Value());
+  mEngine.SetQ((float)(GetParam(kParamQ)->Value() / 100.0));
+  mEngine.SetBallade((float)(GetParam(kParamBallade)->Value() / 100.0));
+  mEngine.SetHorizon((float)(GetParam(kParamHorizon)->Value() / 100.0));
+  mEngine.SetSkew((float)GetParam(kParamSkew)->Value());
+
+  mEngine.RebuildIfNeeded();
+
+  mCurveUISize = mEngine.GetSize();
+  const float* curve = mEngine.GetCurve();
+  for (int i = 0; i < mCurveUISize; i++)
+    mCurveUIBuf[i] = curve[i];
+  mCurveUIUpdated.store(true);
+}
+
+void OperateursSpectraux::OnReset()
+{
+  UpdateEngine();
+}
+
+void OperateursSpectraux::OnParamChange(int paramIdx)
+{
+  switch (paramIdx)
+  {
+    case kParamCycles:
+    case kParamQ:
+    case kParamBallade:
+    case kParamHorizon:
+    case kParamSkew:
+      UpdateEngine();
+      break;
+    default:
+      break;
   }
 }
+
+void OperateursSpectraux::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  // Etape 1 : passthrough pur, aucun traitement audio pour l'instant.
+  for (int i = 0; i < nFrames; i++)
+  {
+    outputs[0][i] = inputs[0][i];
+    outputs[1][i] = inputs[1][i];
+  }
+}
+
 #endif
--- OperateursSpectraux/OperateursSpectraux.h
+++ OperateursSpectraux/OperateursSpectraux.h
@@ -1,35 +1,53 @@
 #pragma once
 
 #include "IPlug_include_in_plug_hdr.h"
+#include "SpectralCurveEngine.h"
+#include "SpectralCurvePreviewControl.h"
+#include <atomic>
 
-const int kNumPresets = 1;
+// ============================================================================
+// Etape 1 : moteur de courbe partage seul (Cycles/Q/Ballade/Horizon/Skew),
+// avec apercu visuel - PAS de traitement audio pour l'instant, juste pour
+// valider le comportement mathematique avant de le brancher sur un vrai
+// module (Filtre/Delay/Inverse Comp).
+// ============================================================================
 
 enum EParams
 {
-  kParamGain = 0,
+  kParamCycles = 0,
+  kParamQ,
+  kParamBallade,
+  kParamHorizon,
+  kParamSkew,
   kNumParams
-};
-
-enum ECtrlTags
-{
-  kCtrlTagVersionNumber = 0,
-  kCtrlTagSlider,
-  kCtrlTagTitle
 };
 
 using namespace iplug;
 using namespace igraphics;
 
-class OperateursSpectraux final : public Plugin
+class OperateursSpectraux final : public iplug::Plugin
 {
 public:
   OperateursSpectraux(const InstanceInfo& info);
 
-#if IPLUG_EDITOR
-  bool OnHostRequestingSupportedViewConfiguration(int width, int height) override { return true; }
-#endif
-  
-#if IPLUG_DSP // http://bit.ly/2S64BDd
+  void OnIdle() override;
+  void OnUIClose() override { mCurveView = nullptr; }
+
+#if IPLUG_DSP
   void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+  void OnParamChange(int paramIdx) override;
+  void OnReset() override;
+#endif
+
+private:
+  SpectralCurvePreviewControl* mCurveView = nullptr;
+
+#if IPLUG_DSP
+  void UpdateEngine();
+
+  SpectralCurveEngine mEngine;
+  std::atomic<bool> mCurveUIUpdated { false };
+  float mCurveUIBuf[512] = { 0.f };
+  int mCurveUISize = 0;
 #endif
 };
